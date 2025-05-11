b4d38b0190af620c3da93aed458ea8519d84a481
adnanfrd <adnanmadhar74@gmail.com>
2025-05-11 15:16:15 +0500

    first commit

---
+++ app/+not-found.tsx
@@ -0,0 +1,11 @@
+
+
+export default function NotFoundScreen() {
+  return (
+    <div>
+    Hi i'm not found page 
+    </div>
+  );
+}
+
+
---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+
+  export default function RootLayout() {
+  
+    return (
+      <div>
+        hi from layout page ....
+      </div>
+    );
+  }
