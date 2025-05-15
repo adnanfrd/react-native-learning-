51f0ba712acd50f50ba6a56a5135a6bf669fd070
adnanfrd <adnanmadhar74@gmail.com>
2025-05-15 10:20:08 +0500

    updated navigation code in layout index.ts

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>🏠 Welcome to Home</Text>
+      <Button title="Go to Profile" onPress={() => router.push('/todolist/TodoList')} />
+    </View>
+  );
+}
